92688d0b381155973edbcfab1b9658d40a1a4c79
eirivarn <eirik.varnes@gmail.com>
2024-03-12 18:40:38 +0100

    Task 4 a done 4 real

--- assignment4/SSD/ssd/modeling/backbones/basic.py
+++ assignment4/SSD/ssd/modeling/backbones/basic.py
@@ -1,7 +1,6 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from typing import Tuple, List
+
 
 class BasicModel(torch.nn.Module):
     """
@@ -22,55 +21,77 @@ class BasicModel(torch.nn.Module):
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
 
-        # Define convolutional layers
-        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.block_1 = torch.nn.Sequential(
+            torch.nn.Conv2d(image_channels, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(64, output_channels[0], kernel_size=3, stride=2, padding=1),
+            torch.nn.ReLU()
+        )
 
-        # Define max pooling layers
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.block_2 = torch.nn.Sequential(
+            torch.nn.Conv2d(output_channels[0], 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, output_channels[1], kernel_size=3, stride=2, padding=1),
+            torch.nn.ReLU()
+        )
+        
+        self.block_3 = torch.nn.Sequential(
+            torch.nn.Conv2d(output_channels[1], 256, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(256, output_channels[2], kernel_size=3, stride=2, padding=1),
+            torch.nn.ReLU()
+        )
+
+        self.block_4 = torch.nn.Sequential(
+            torch.nn.Conv2d(output_channels[2], 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, output_channels[3], kernel_size=3, stride=2, padding=1),
+            torch.nn.ReLU()
+        )
+        
+        self.block_5 = torch.nn.Sequential(
+            torch.nn.Conv2d(output_channels[3], 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, output_channels[4], kernel_size=3, stride=1, padding=0),
+            torch.nn.ReLU()
+        )
+        
+        self.block_6 = torch.nn.Sequential(
+            torch.nn.Conv2d(output_channels[4], 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, output_channels[5], kernel_size=3, stride=1, padding=0)
+        )
+        
+        self.blocks = [self.block_1, self.block_2, self.block_3, self.block_4, self.block_5, self.block_6]
+        
 
     def forward(self, x):
         """
         The forward function should output features with shape:
             [shape(-1, output_channels[0], 38, 38),
-            shape(-1, output_channels[1], 19, 19),
-            shape(-1, output_channels[2], 10, 10),
-            shape(-1, output_channels[3], 5, 5),
-            shape(-1, output_channels[3], 3, 3),
-            shape(-1, output_channels[4], 1, 1)]
-        We have added assertion tests to check this, iteration through out_features,
-        where out_features[0] should have the shape:
-            shape(-1, output_channels[0], 38, 38),
+             shape(-1, output_channels[1], 19, 19),
+             shape(-1, output_channels[2], 10, 10),
+             shape(-1, output_channels[3], 5, 5),
+             shape(-1, output_channels[3], 3, 3),
+             shape(-1, output_channels[4], 1, 1)]
         """
         out_features = []
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        out_features.append(x)
-        x = self.pool(x)
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        out_features.append(x)
-        x = self.pool(x)
-        x = F.relu(self.conv5(x))
-        out_features.append(x)
-        x = self.pool(x)
-        x = F.relu(self.conv6(x))
-        out_features.append(x)
-        x = self.pool(x)
-
+        for block in self.blocks:
+            out_features.append(block(x))
+            x = out_features[-1]
+            
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
-            expected_shape = (feature.size(0), out_channel, h, w)
-            assert feature.shape[1:] == expected_shape[1:], \
+            expected_shape = (out_channel, h, w)
+            assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
-        assert len(out_features) == len(self.output_feature_shape), \
+        assert len(out_features) == len(self.output_feature_shape),\
             f"Expected that the length of the outputted features to be: {len(self.output_feature_shape)}, but it was: {len(out_features)}"
-
         return tuple(out_features)
-
-
